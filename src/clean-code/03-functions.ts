0e83783c49d89c9472266a261e0f049e2834676b
rromerro900814094 <rromeror@indracompany.com>
2023-10-17 14:53:50 -0500

    Adicionar funcion complicada

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -33,4 +33,24 @@
 
     }
 
+    // Función complicada
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        let result;
+        if ( isDead ) {
+            result = 1500;
+        } else {
+            if ( isSeparated ) {
+                result = 2500;
+            } else {
+                if ( isRetired ) {
+                    result = 3000;
+                } else {
+                    result = 4000; 
+                }
+            }
+        }
+        
+        return result;
+    }
+
 })();
