ea8ebb405e4135dc3357e2fd770462a8e245ff5c
rromerro900814094 <rromeror@indracompany.com>
2024-01-11 16:28:00 -0500

    Definicion corta clase

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -2,15 +2,7 @@
 
     type Gender = 'M'|'F';
     class Person {
-        public name: string;
-        public gender: Gender;
-        public birthdate: Date;
-
-        constructor(name:string, gender: Gender, birthdate: Date) {
-            this.name = name;
-            this.gender = gender;
-            this.birthdate = birthdate;
-        }
+        constructor(public name:string, public gender: Gender, public birthdate: Date) { }
     }
 
     const newPerson = new Person('Yotas', 'M', new Date('2013-05-23'));
